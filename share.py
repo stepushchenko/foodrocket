4fa228691fd1b08577b3cd39ffbde73933e12ae2
stepushchenko <ilia.stepushchenko@gmail.com>
2022-10-27 22:00:42 +0200

    move test data from parameters to the data.yaml file

--- share.py
+++ share.py
@@ -6,4 +6,7 @@ import yaml
 with open("configuration.yaml", "r") as file:
     configuration = yaml.load(file, Loader=yaml.FullLoader)
 
+with open("data.yaml", "r") as file:
+    data = yaml.load(file, Loader=yaml.FullLoader)
+
 ROOT_DIR = dirname(realpath(__file__))
--- test_cases/test_footer.py
+++ test_cases/test_footer.py
@@ -2,6 +2,7 @@
 import ipdb
 import pytest
 
+import share
 # internal imports
 from pages.base_page import BasePage
 from pages.footer_page import FooterPage
@@ -13,15 +14,15 @@ class TestFooter:
     def test_link_to_contact_us_button(self, driver, env):
         browser = BasePage(driver, env)
         browser.open('')
-        browser.is_href_present(FooterPage.contact_us_button, "mailto:support@foodrocket.me")
+        browser.is_href_present(FooterPage.contact_us_button, share.data['contact_us_email'])  # todo: move parameter value to the configuration.yaml
 
     def test_link_to_app_store_button(self, driver, env):
         browser = BasePage(driver, env)
         browser.open('')
-        browser.is_href_present(FooterPage.app_store_button, "https://apps.apple.com/app/food-rocket-grocery-delivery/id1552202489")
+        browser.is_href_present(FooterPage.app_store_button, share.data['app_store_download_link'])
 
     def test_link_to_google_play_button(self, driver, env):
         browser = BasePage(driver, env)
         browser.open('')
-        browser.is_href_present(FooterPage.google_play_button, "https://play.google.com/store/apps/details?id=me.foodrocket.app")
+        browser.is_href_present(FooterPage.google_play_button, share.data['google_play_market_download_link'])
 
